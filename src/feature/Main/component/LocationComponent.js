e2b39cd58123b9103aa59d3d3a7488acce6b6278
Kaung Min Khant <kaungminkhant05723123@gmail.com>
2024-01-25 22:46:16 +0630

    Fixing websocket connection management_1

--- src/feature/Main/component/LocationComponent.js
+++ src/feature/Main/component/LocationComponent.js
@@ -1,19 +1,26 @@
-import "bulma/css/bulma.min.css"
-import "../../../index.css"
+import "bulma/css/bulma.min.css";
+import "../../../index.css";
 
 function LocationComponent({ onChange }) {
-
-    return (
-        <div className="location-layout">
-            <div className="select is-rounded is-danger">
-                <select className="is-focus" onChange={event => onChange(event.target.value)}>
-                    <option value={""} >Location</option>
-                    <option value={"South Okkalapa"}>South Okkalapa</option>
-                    <option value={"Insein"}>Insein</option>
-                </select>
-            </div>
-        </div>
-    )
+  return (
+    <div className="location-layout">
+      <div className="select is-rounded is-danger">
+        <select
+          className="is-focus"
+          onChange={(event) => onChange(event.target.value)}
+        >
+          <option value={""}>Location</option>
+          <option value={"၁၀ ရပ်ကွက်စျေး"}>၁၀ ရပ်ကွက်စျေး</option>
+          <option value={"ပဒေသာလမ်းစုံ"}>ပဒေသာလမ်းစုံ</option>
+          <option value={"မေတ္တာလမ်း ဓမ္မာရုံ"}>မေတ္တာလမ်း ဓမ္မာရုံ</option>
+          <option value={"ဆရာလေးကျောင်း"}>ဆရာလေးကျောင်း</option>
+          <option value={"ရတနာလမ်း"}>ရတနာလမ်း</option>
+          <option value={"ဂိတ်ဟောင်း"}>ဂိတ်ဟောင်း</option>
+          <option value={"အထင်ကရ"}>အထင်ကရ</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default LocationComponent;
